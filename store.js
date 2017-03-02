0f51db70a7e836d11b881ef00fdf11d796112dec
Mr_Secunda <alexander.snesar@kredito.de>
2017-03-02 17:57:42 +0200

    removed dev tools

--- store.js
+++ store.js
@@ -10,5 +10,4 @@ const middleware = applyMiddleware(promise(), thunk, logger())
 
 export default createStore(
     reducer, middleware,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
